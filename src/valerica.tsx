bfc29000922402ef05c78e32ee14f691debe1274
Valerica <valerica@example.com>
2026-02-04 13:27:21 +0200

    Adaug fisier personal valerica

---
+++ src/valerica.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Fișier personal pentru Valerica - poți scrie codul tău aici
+// Acest fișier este pe branch-ul valerica și nu afectează fișierul principal main.tsx
+
+const ValericaComponent: React.FC = () => {
+  return (
+    <div>
+      <h1>Bună, Valerica!</h1>
+      <p>Aici poți scrie codul tău personal.</p>
+      {/* Adaugă codul tău aici */}
+    </div>
+  );
+};
+
+export default ValericaComponent;
